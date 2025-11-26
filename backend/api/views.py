952bc72b75371a0627920b05cfd9b2551cc29c55
Fazlul001 <fazlul.faizal.802@my.csun.edu>
2025-11-25 17:47:23 -0800

    added itentifier

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,13 +6,11 @@ from django.http import JsonResponse
 def hello(request):
     return JsonResponse({"message": "Hello World"})
 
-def login(request):
+def login(request, user_identifier):
     username = request.POST.get('email')
     password = request.POST.get('password')
 
-    
-
-    data = { 'message' : f'{username}, {password}' }
+    data = { 'message' : f'{username}, {password}, {user_identifier}' }
     
     return JsonResponse(data)
 
