5ab14b021e02932ec32e70f688cbb5eb32f4b270
Fazlul001 <fazlul.faizal.802@my.csun.edu>
2025-12-02 21:25:52 -0800

    parsed put request

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+import json
 from django.http import JsonResponse
-
+from django.views.decorators.csrf import csrf_exempt
 def hello(request):
     return JsonResponse({"message": "Hello World"})
 
+@csrf_exempt
 def login(request, user_identifier):
-    return JsonResponse(request.body)
+    print(f"Logging in user: {user_identifier}")
+    return JsonResponse({"status": 400})
